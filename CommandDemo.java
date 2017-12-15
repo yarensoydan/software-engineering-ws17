674e1e7647ce9874f180cd8c7489220a7bd8aae1
yarensoydan <33071519+yarensoydan@users.noreply.github.com>
2017-12-15 04:13:26 +0300

    Update CommandDemo.java

--- CommandDemo.java
+++ CommandDemo.java
@@ -1,1 +1,32 @@
+package se2;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandDemo {
+	
+	public static List<Command> produceRequests()  {
+		
+		List<Command> queue = new ArrayList<>();
+		queue.add(new ConvertMetertoMilimeter());
+		queue.add(new ConvertMetertoCentimeter());
+		queue.add(new ConvertFahrenheittoCelsius());
+		queue.add(new ConvertCelsiustoFahrenheit());
+		queue.add(new ConvertDollartoTRY());
+		queue.add(new ConvertDollartoCNY());
+		queue.add(new ConvertDollartoEuro());
+		return queue;
+	}
+	
+	
+	public static void  workoffRequests(List<Command> queue) {
+		
+		for (Object command : queue) { ((Command)command).execute();}
+		}
+
+  public static void main (String[] args ) {
+	  List<Command> queue = produceRequests();
+	  workoffRequests(queue);
+	  }
+  }
+ 
