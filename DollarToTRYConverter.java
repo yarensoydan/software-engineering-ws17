7026993f689a65a8701481f234c94e74a10343c8
yarensoydan <33071519+yarensoydan@users.noreply.github.com>
2017-12-15 04:31:10 +0300

    Create DollarToTRYConverter.java

---
+++ DollarToTRYConverter.java
@@ -0,0 +1,20 @@
+package se2;
+
+public class DollarToTRYConverter extends CurrencyConverter{
+
+    public double originalValue;
+    public double convertedValue;
+    public DollarToTRYConverter() {}
+    
+    public double convert(double inDollars) 
+    {
+    	originalValue=inDollars;
+        convertedValue=3.83*originalValue;
+        return convertedValue;
+    }
+
+    public String toString(){return originalValue+"USD ="+convertedValue + "TRY";}
+    public void print(){System.out.println(toString());}
+
+}
+
