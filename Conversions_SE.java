8eeb4348923507d54b95cb7a6a428b00ca7b9872
yarensoydan <33071519+yarensoydan@users.noreply.github.com>
2017-12-15 04:08:21 +0300

    Update and rename Conversions_SE. to Conversions_SE.java

---
+++ Conversions_SE.java
@@ -0,0 +1,10 @@
+
+package se2;
+
+public class Conversions_SE {
+	public void LengthConverter() { System.out.println("Conversion: The lenght has converted");}
+	public void CurrencyConverter() { System.out.println("Conversion: The currency has converted");}
+	public void TemperatureConverter() { System.out.println("Conversion: The temperature has converted");}
+	
+
+}
