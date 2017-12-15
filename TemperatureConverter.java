c70c237bbec4bf232fa4bd8ce249025dd693c550
yarensoydan <33071519+yarensoydan@users.noreply.github.com>
2017-12-15 04:35:38 +0300

    Create TemperatureConverter.java

---
+++ TemperatureConverter.java
@@ -0,0 +1,14 @@
+package se2;
+
+
+public class TemperatureConverter extends UnitConverter 
+{
+
+    @Override public double convert(double inValue) { return 0;}
+
+    @Override public String toString() {return "Welcome to Temperature converter"; }
+
+    @Override public void print() {System.out.println(toString()); }
+
+}
+
