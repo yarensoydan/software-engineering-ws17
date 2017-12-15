d866ab5d792eb3027bf90a66ff1ba11d22e512e8
yarensoydan <33071519+yarensoydan@users.noreply.github.com>
2017-12-15 04:11:09 +0300

    Update CelsiusToFahrenheitConverter.java

--- CelsiusToFahrenheitConverter.java
+++ CelsiusToFahrenheitConverter.java
@@ -1,1 +1,30 @@
 
+package se2;
+
+public class CelsiusToFahrenheitConverter extends TemperatureConverter {
+
+    public double originalValue;
+    public double convertedValue;
+
+    public CelsiusToFahrenheitConverter() {}
+
+    public double convert(double inCelsius) {
+
+        originalValue=inCelsius;
+        convertedValue=32+1.8*originalValue;
+        return convertedValue;
+
+    }
+    
+    public String toString(){
+        return originalValue+"Celsius ="+convertedValue + "Fahrenheit";
+
+    }
+
+    public void print(){
+        System.out.println(toString());
+    }
+    
+   
+}
+
