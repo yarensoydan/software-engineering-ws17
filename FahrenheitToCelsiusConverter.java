43f25242037daba6d9b5179dde2f41fb37093698
yarensoydan <33071519+yarensoydan@users.noreply.github.com>
2017-12-15 04:32:09 +0300

    Create FahrenheitToCelsiusConverter.java

---
+++ FahrenheitToCelsiusConverter.java
@@ -0,0 +1,24 @@
+package se2;
+
+
+public class FahrenheitToCelsiusConverter extends  TemperatureConverter
+{
+
+    public double originalValue;
+    public double convertedValue;
+    public FahrenheitToCelsiusConverter() {}
+
+    public double convert(double inCelsius) 
+    {
+    	originalValue=inCelsius;
+        convertedValue=(originalValue-32)/1.8;
+        return convertedValue;
+
+    }
+
+    public String toString(){return originalValue+"Fahrenheit ="+convertedValue + "Celsius";}
+
+    public void print(){System.out.println(toString());}
+
+}
+
