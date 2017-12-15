10e0f7151f25bd4a3b8da2aa80f843f5e250b670
yarensoydan <33071519+yarensoydan@users.noreply.github.com>
2017-12-15 04:26:26 +0300

    Create CurrencyConverter.java

---
+++ CurrencyConverter.java
@@ -0,0 +1,19 @@
+package se2;
+
+public class CurrencyConverter extends UnitConverter {
+	@Override
+	public double convert(double inValue) {
+	return 0;
+	}
+	
+	@Override
+	public String toString() {
+	return "Welcome to Currency converter";
+	}
+	
+	@Override
+	public void print() {
+	System.out.println(toString());
+	
+	}
+	}
